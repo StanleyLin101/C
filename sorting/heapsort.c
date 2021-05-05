8d6848181e954e8a1447b6fd05370d2612392964
stanley408410111 <79526975+stanley408410111@users.noreply.github.com>
2021-05-06 00:41:54 +0800

    Create heapsort.c

---
+++ sorting/heapsort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "heapsort.h"
+int main()
+{
+    struct  timeval start;
+    struct  timeval end;
+    unsigned  long diff;
+
+    gettimeofday(&start,NULL);
+
+    FILE *fp;
+    int number;
+    fp = fopen("dataset1.txt", "r");
+    if (fp == NULL)
+    {
+        printf("Can't open file.\n");
+    }
+    else
+    {
+        for(int i=0; i<1000000; i++)
+        {
+            fscanf(fp, "%d", &number);
+            a[i] = number;
+        }
+        fclose(fp);
+    }
+
+    heapSort(a, 1000000);
+
+    printf("sorted array : \n");
+
+    for (int i = 0; i < 1000000; i++)
+        printf("%d\n", a[i]);
+
+    gettimeofday(&end,NULL);
+
+    diff = 1000000*(end.tv_sec-start.tv_sec)+ end.tv_usec-start.tv_usec;
+    float sec;
+    sec=(float)diff/1000000;
+    printf("\nThe time required to sort 1,000,000 data is %f sec\n",sec);
+    return 0;
+}
